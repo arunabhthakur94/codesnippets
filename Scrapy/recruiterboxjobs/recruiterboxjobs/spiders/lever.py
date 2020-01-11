f2f8f14a92e1e16ddac5bc2d71862aa1d454d625
Arunabh Singh <arunabh.thakur94@gmail.com>
2020-01-11 19:06:40 +0530

    Parsed Jobs From Lever.co

--- Scrapy/recruiterboxjobs/recruiterboxjobs/spiders/lever.py
+++ Scrapy/recruiterboxjobs/recruiterboxjobs/spiders/lever.py
@@ -49,20 +49,24 @@ class CrawlLeverJobs(scrapy.Spider):
 
 class crawlLever(scrapy.Spider):
     name = 'leverscraper'
-    start_urls = ['file////home/masai/Documents/lever/innerpage1.html']
+    start_urls = ['file:///home/arunabh/codebase/Project/Scrapy/recruiterboxjobs/recruiterboxjobs/lever/level1Crawl/innerpage1.html']
 
     next_job = 1
+    count = 1
 
     def parse(self, response):
         jobs = response.xpath('//a[contains(@href, "jobs")]/@href').getall()
         for i in jobs:
             yield response.follow(i, callback=self.parseJob)
+        
+        crawlLever.next_job += 1
+        go_next = 'file:///home/arunabh/codebase/Project/Scrapy/recruiterboxjobs/recruiterboxjobs/lever/level1Crawl/innerpage'+str(crawlLever.next_job)+'.html'
+        yield response.follow(go_next, callback = self.parse)
     
     def parseJob(self, response):
         # open_in_browser(response)
-        count = 1
         job_title = response.xpath('//h2/text()').extract_first()
-        company_name = response.xpath('').extract()
+        company_name = response.xpath('/html/body/div[2]/div/div/a/img/@alt').extract()
         description = response.xpath('//div[(((count(preceding-sibling::*) + 1) = 7) and parent::*)]/text()').extract()
         payscale = 'Best In Industry'
         location=response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "sort-by-time", " " ))]/text()').extract_first()
@@ -71,17 +75,17 @@ class crawlLever(scrapy.Spider):
         date_posted = 'Yesterday'
         parent_source = response.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "black", " " ))]/@href').extract_first()
         active = 'True'
-        if len(job_type) == 0:
+        if job_type is None:
             job_type = 'Full-time'
-        if len(description) == 0:
+        if description is None:
             description = 'Our'+job_title+' will be the master of all things content execution and strategy. Your objective will be to generate content that is aligned with our positioning, thought leadership, Inbound and demand generation strategy. You will need to conduct user persona research, competitive research, keyword research, topic research, create a list of content topics and organize them in a content calendar, write some compelling content yourself, assign the right content topic(s) to the right freelancer/ agency, supervise and approve their work, make sure content is properly linked internally and externally, launch the content on our website / blog/ email list/ newsletter, track traffic and conversion overtime and assist with tweaking content to improve conversion. Own the impact content has on site traffic, demo request/ trial conversion and wins. The role will report to the VP-Product Marketing who reports to the CEO.'
 
         with open ('../leverjobs.csv','a') as csvfile:
             fieldnames = ['job_title', 'company_name', 'description', 'payscale', 'location', 'job_type', 'company_type', 'date_posted', 'parent_source', 'active']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            if count == 1:
+            if crawlLever.count == 1:
                 writer.writeheader()
-                count += 1
+                crawlLever.count += 1
             writer.writerow({
                 'job_title': job_title,
                 'company_name': company_name,
